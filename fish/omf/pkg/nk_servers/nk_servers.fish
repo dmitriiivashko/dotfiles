0d8916d55ecfb62857f22f957363cf2c5177388d
Dmitrii Ivashko <thetrancedj@gmail.com>
2015-12-01 15:20:45 +0300

    php paths

--- fish/omf/pkg/nk_servers/nk_servers.fish
+++ fish/omf/pkg/nk_servers/nk_servers.fish
@@ -3,3 +3,4 @@ function mysql.stop; mysql.server stop; end;
 function redis.start; redis-server /usr/local/etc/redis.conf; end;
 function redis.stop; redis-cli shutdown; end;
 
+set PATH /usr/local/opt/php56/bin $PATH
